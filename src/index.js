bdc86ce9db4f10963cce817232be5c5c2d24f61b
w28ahmad <43382198+w28ahmad@users.noreply.github.com>
2019-05-13 06:58:25 -0400

    Adding multer for file uploads and creating a avatars folder

--- src/index.js
+++ src/index.js
@@ -7,6 +7,16 @@ const taskRouter = require('./routers/tasks')
 const app = express()
 const port = process.env.port || 3000
 
+// const multer = require('multer')
+// const upload = multer({
+//     dest:'images'
+// })
+
+// app.post('/upload', upload.single('upload'), (req,res)=>{
+
+//     res.send()
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -15,32 +25,3 @@ app.listen(port, ()=>{
     console.log("Server is up on port "+ port)
 })
 
-
-// const jwt = require('jsonwebtoken')
-
-// myFunc = async() =>{
-//     const token = jwt.sign({_id: 'abc123'}, 'thisismynewcourse', {expiresIn: "10 second"})
-//     console.log(token)
-
-//     const data = jwt.verify(token, 'thisismynewcourse')
-//     console.log(data)
-// }
-
-// myFunc()
-
-const Task = require('./models/tasks')
-const User = require('./models/user')
-
-const main = async () =>{
-    // const task = await Task.findById('5cd8aa3593b12645ec82bbfc')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    const user = await User.findById('5cd8ae8f94127c190cccf63e')
-    await user.populate('tasks').execPopulate()    
-    console.log(user.tasks)
-
-}
-
-
-// main()
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const multer = require('multer')
+
 const router = new express.Router()
 
 router.post('/users', async(req, res)=>{
@@ -88,6 +90,14 @@ router.post('/users/logoutAll', auth, async (req, res)=>{
     }catch(e){
         res.status(500).send()
     }
+})
+
+const upload = multer({
+    dest:'avatars'
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req,res)=>{
+    res.send()
 })
 
 module.exports = router

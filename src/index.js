7146a8fb60af090b8a2d240cf1492429d68b1fe3
w28ahmad <43382198+w28ahmad@users.noreply.github.com>
2019-05-14 17:38:07 -0400

    Adding sharp for auto cropping the images and conforming the file type to png

--- src/index.js
+++ src/index.js
@@ -7,27 +7,6 @@ const taskRouter = require('./routers/tasks')
 const app = express()
 const port = process.env.port || 3000
 
-const multer = require('multer')
-const upload = multer({
-    dest:'images',
-    limits:{
-        fileSize: 1000000,
-    },
-    fileFilter(req, file, callback){
-        if(!file.originalname.match(/\.(doc|docx)$/)){
-            return callback(new Error("Please upload a word document"))
-        }
-        callback(undefined, true)
-    }
-})
-
-app.post('/upload', upload.single('upload'), (req,res)=>{
-
-    res.send()
-}, (error, req, res, next)=>{
-    res.status(400).send({error: error.message})
-})
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
